52e61c5e369c63b8260bf7bba20795282048dfd9
Billiam <billiamthesecond@gmail.com>
2014-02-13 00:21:21 -0600

    Updating block styles for headers / code sections.
    Issue #4

--- lib/fdoc/presenters/json_presenter.rb
+++ lib/fdoc/presenters/json_presenter.rb
@@ -18,7 +18,7 @@ class Fdoc::JsonPresenter
       '<code>%s</code>' % json
     elsif json.kind_of?(Hash) ||
           json.kind_of?(Array)
-      '<div class="CodeRay code-section example-body"><pre>%s</pre></div>' % CodeRay.scan(JSON.pretty_generate(json), :json).div(wrap: nil, css: :class)
+      '<pre class="CodeRay">%s</pre>' % CodeRay.scan(JSON.pretty_generate(json), :json).div(wrap: nil, css: :class)
     end
   end
 
